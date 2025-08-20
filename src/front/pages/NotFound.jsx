2a3ce986501c7027ec5b94747e77ae45434dc900
LeslieGG <ggarciaveka@gmail.com>
2025-08-19 23:25:58 -0500

    404 and terms and conditions

---
+++ src/front/pages/NotFound.jsx
@@ -0,0 +1,16 @@
+export default function NotFound() {
+    return (
+        <div>
+            <div className="bg-success text-white">Nav bar here</div>
+            <div className=" d-flex justify-content-center">
+                <div className="d-flex justify-content-center align-items-center flex-column">
+                    <img src="https://cdn-icons-png.flaticon.com/512/2885/2885344.png" alt="404 Not found" />
+                    <h1 className="fw-bolder fs-1">404</h1>
+                    <h1>Sorry, page Not Found</h1>
+                </div>
+            </div>
+
+        </div>
+
+    )
+}
