e6e4ec9e4ea931fa7d3a837e41bf70165379df67
LeslieGG <ggarciaveka@gmail.com>
2025-08-19 22:45:19 -0500

    ...

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react"
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
-import TourCard from "../components/TourCard.jsx";
 
 export const Home = () => {
 
@@ -36,7 +35,6 @@ export const Home = () => {
 	return (
 		<div className="text-center mt-5">
 			<h1 className="display-4">Hello Rigo!!</h1>
-			<TourCard title='Hola perro' img='https://media.admagazine.com/photos/618a65f329327ed3de99d73b/master/w_1600%2Cc_limit/72394.jpg' description='descrip' />
 			<p className="lead">
 				<img src={rigoImageUrl} className="img-fluid rounded-circle mb-3" alt="Rigo Baby" />
 			</p>
